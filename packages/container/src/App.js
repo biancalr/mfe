ed808e404a2096339e80bf8bde8eed7550f47ef3
biancalr <biancalr@github.com>
2025-07-11 17:51:46 -0300

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/marketingApp";
+
+export default () => {
+  return (
+    <div>
+      <h1>Container</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
